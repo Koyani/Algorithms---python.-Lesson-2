c426ef0dc18efc56ee4ce38b7435b517ac2b59fb
Koyani <jabox@yandex.ru>
2018-12-25 09:51:11 +0300

    Update task_7.py

--- task_7.py
+++ task_7.py
@@ -1,14 +1,12 @@
 #task_7
 #Напишите программу, доказывающую или проверяющую, 
 #что для множества натуральных чисел выполняется равенство: 1+2+...+n = n(n+1)/2, где n - любое натуральное число.
-
-
-n = int(input("Enter any natural number: "))
+number = int(input("Enter any natural number: "))
 x = 0
-for i in range(1,n+1):
+y = number * (number + 1) // 2
+for i in range(1, number + 1):
     x += i
-m = n * (n + 1) // 2
-if x==m:
+if x==y:
     print("1+2+...+n = n(n+1)/2")
 else:
     print("1+2+...+n != n(n+1)/2")
